98be1d37b298af576ebde4310130d606b9387f87
jahidhasan6484 <jahidhasan6484>
2023-12-26 21:38:01 +0600

    master is clean now

--- dist/function.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const add = (a, b) => {
-    return a + b;
-};
-console.log(add(2, 3));
-const numbers = [1, 2, 3, 4];
-const square_of_numbers = numbers.map((number) => number * number);
-console.log("HII");
--- dist/object.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const mySelf = {
-    name: "Jahid Hasan",
-    age: 22,
-    is_married: false,
-};
-console.log(mySelf.is_married);
